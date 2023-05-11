c747e04185836fcd04c9b92f6d2eaa15cb807e5d
Moises Garcia <moisesgarciafalcon@hotmail.com>
2023-05-11 01:34:45 -0700

    Octavo Commit, Login y register funcionando, menu basico y visual

--- JS/register.js
+++ JS/register.js
@@ -1,22 +1,7 @@
-import {app,auth,database,createUser} from './module.js';
 
-signUp.addEventListener('click',(e) =>{
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let username = document.getElementById('username').value;
+const cambiarLogin = (a) =>{
+  window.location.href = a;
+}
 
-    //Auth
-    createUser(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        alert('Registro exitoso');
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert('errorMessage');
-        // ..
-      });
-})
+export default cambiarLogin;
+
